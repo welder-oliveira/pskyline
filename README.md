a8860e4c743243e1f823501c694966fcce808740
Welder <welder.oliveira@cuia.com.br>
2016-12-21 12:13:23 -0200

    Casting coordinates to numeric. Including the sample school data

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # pskyline
-Compute p-skylines for several probability distributions for positional uncertain spatial data
+
+## Description
+A program which implements a spatial p-skyline, i.e. one that receives as INPUT: 
+a) a set S of points with imprecise coordinates, 
+b) a threshold probability p which is the minimum non dominance probability 
+of every point which will be in the final solution;
+and OUTPUTs the p-skyline subset R from S, i.e. the set of all points in S
+with a Pareto efficience probability at least equal to p
+
+## Arguments
+### mandatory
+tab: a data.frame with each line representing a candidate to be evaluated by pSkyline
+latCol: column number for the latitudes of the spatial
+lonCol: column number for the longitudes of the spatial
+meanError: the mean error in METERS in the coordinates
+sdError: the standard error in METERS in the coordinates
+
+### recommended but not mandatory 
+p:      the threshold probability required for a spatial data object to be in the 
+        skyline. By default 0.5
+refLat, refLon: arrays of reference coordinates of points from which the distance of 
+         each dataset spatial object must be computed. If NULL is passed 
+         (default), the coordinates of the spatial object in the FIRST ROW are 
+         used as reference coordinates.
+degreeNotation: TRUE if the coordinates are in degree notation (default). 
+         FALSE if they are in radian
+pdfSpatial: pdf option for error in the coordinates (default = "normal")
+
+## Details
+The algorithm proceeds by deriving new columns to each reference point. The 
+distances of each spatial object i is computed to each reference point and these 
+values are stored in the derived columns. THOSE derived values are used after to 
+compute the spatial p-skyline under the criterium of minimization. Thus the goal is 
+to find those spatial points which are the nearest ones with respect to the 
+references points.
+----------
+The pdf options are: "normal" (default), "exponencial" and "chisquare". These 
+names also may be provides as "norm", "expo", "exp" and "chisq".
--- pSkyline.R
+++ pSkyline.R
@@ -1,43 +1,5 @@
 # Spatial pSkyline
 
-# Description
-# A program which implements a spatial p-skyline, i.e. one that receives as INPUT: 
-# a) a set S of points with imprecise coordinates, 
-# b) a threshold probability p which is the minimum non dominance probability 
-# of every point which will be in the final solution;
-# and OUTPUTs the p-skyline subset R from S, i.e. the set of all points in S
-# with a Pareto efficience probability at least equal to p
-
-# Arguments
-# ---------- mandatory ----------
-# tab: a data.frame with each line representing a candidate to be evaluated by pSkyline
-# latCol: column number for the latitudes of the spatial
-# lonCol: column number for the longitudes of the spatial
-# meanError: the mean error in METERS in the coordinates
-# sdError: the standard error in METERS in the coordinates
-
-# ---------- recommended but not mandatory ----------
-# p:      the threshold probability required for a spatial data object to be in the 
-#         skyline. By default 0.5
-# refLat, refLon: arrays of reference coordinates of points from which the distance of 
-#             each dataset spatial object must be computed. If NULL is passed 
-#             (default), the coordinates of the spatial object in the FIRST ROW are 
-#             used as reference coordinates.
-# degreeNotation: TRUE if the coordinates are in degree notation (default). 
-#                 FALSE if they are in radian
-# pdfSpatial: pdf option for error in the coordinates (default = "normal")
-
-# Details
-# The algorithm proceeds by deriving new columns to each reference point. The 
-# distances of each spatial object i is computed to each reference point and these 
-# values are stored in the derived columns. THOSE derived values are used after to 
-# compute the spatial p-skyline under the criterium of minimization. Thus the goal is 
-# to find those spatial points which are the nearest ones with respect to the 
-# references points.
-# ----------
-# The pdf options are: "normal" (default), "exponencial" and "chisquare". These 
-# names also may be provides as "norm", "expo", "exp" and "chisq".
-
 library(oce)
 localPskylineFolder = "/home/welder/Documentos/projetos/pskyline"
 source(paste(localPskylineFolder, "sphericalDistance.R", sep = "/"))
@@ -54,6 +16,10 @@ pSkyline <- function (tab, latCol, lonCol, meanError, sdError, p = 0.5,
     refLon = tab[1, lonCol]
   }
     
+  # Cast coordinates to numeric
+  tab[, latCol] = as.numeric(as.character((tab[, latCol])))
+  tab[, lonCol] = as.numeric(as.character((tab[, lonCol])))
+  
   P = c()   # set of Pareto efficient points 
   countOfParetoEfficientPoints = 0
   probParetoEfficient = numeric(nrow(tab))
@@ -164,8 +130,8 @@ metersToDegrees <- function(x) {
 }
 
 # Example
-pathSchool = "/home/welder/Documentos/layers/36_escolas_goiania.csv"
-tab = read.csv(pathSchool)
+pathSchool = paste(localPskylineFolder, "sampleData_36gynSchools", sep = "/")
+tab = read.csv2(pathSchool)
 latCol = 2
 lonCol = 3
 refLat = c(tab[1, 2], tab[2, 2])
